0c54197ca6e6b457bd7b3e8841aa77ab736072df
FabianMolinaEE <fabianmolina@stanford.edu>
2025-04-18 18:00:04 -0700

    git fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/',
+  base: mode === 'development' ? '/' : 'https://fabianmolinaee.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
